1b3a44cd3b6a6a9640d697997c9774a003e13875
tomg10 <lemures64@gmail.com>
2024-03-14 01:43:31 +0100

    metrics, config and various fixes
    Signed-off-by: tomg10 <lemures64@gmail.com>

--- core/bin/external_node/src/config/mod.rs
+++ core/bin/external_node/src/config/mod.rs
@@ -253,10 +253,12 @@ pub struct OptionalENConfig {
 
     #[serde(default = "OptionalENConfig::default_l1_batch_commit_data_generator_mode")]
     pub l1_batch_commit_data_generator_mode: L1BatchCommitDataGeneratorMode,
-    //
-    // pub pruning_chunk_size: u64,
-    //
-    // pub min_block_age_to_prune_hours: u64,
+
+    #[serde(default = "OptionalENConfig::default_pruning_chunk_size")]
+    pub pruning_chunk_size: u32,
+
+    /// If set, l1 batches will be pruned after they are that long
+    pub l1_batch_age_to_prune_hours: Option<u64>,
 }
 
 impl OptionalENConfig {
@@ -373,6 +375,10 @@ impl OptionalENConfig {
 
     const fn default_l1_batch_commit_data_generator_mode() -> L1BatchCommitDataGeneratorMode {
         L1BatchCommitDataGeneratorMode::Rollup
+    }
+
+    const fn default_pruning_chunk_size() -> u32 {
+        10
     }
 
     pub fn polling_interval(&self) -> Duration {
